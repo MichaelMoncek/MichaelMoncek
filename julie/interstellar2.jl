7497b0a252ceceb91b9eac8c9e79c2fda6bffa7f
mmoncek <michael.moncek@seznam.cz>
2025-08-12 20:05:33 +0200

    Fixed the coordinate system orientation

--- julie/interstellar2.jl
+++ julie/interstellar2.jl
@@ -11,23 +11,23 @@ using Parameters
 const W = 100 # horizontal resolution 
 const H = 100 # vertical resolution 
 # camera
-const camera_position = [0.0, 1.0, -3.0] # camera position in space
+const camera_position = [0.0, 1.0, 3.0] # camera position in space
 const fov = π/3 # field of view
 const look_at = [0.0, 0.0, 0.0] # point the camera is looking at 
-const world_up = [0.0, 1.0, 0.0] # world up vector 
+const world_up = [1.0, 0.0, 0.0] # world up vector 
 # sphere and disc
 const sphere_position = [0.0, 0.0, 0.0] # center of the sphere 
 const sphere_radius = 0.5 # radius of the sphere 
-const second_sphere_position = [0.0, 0.0, 1.0]
+const second_sphere_position = [1.0, 1.0, -1.0]
 const second_sphere_radius = 0.7 
-const third_sphere_position = [1.0, -1.0, 1.0]
+const third_sphere_position = [-1.0, 2.0, -3.0]
 const third_sphere_radius = 0.3
 const Rin = 0.6 # inner radius of the disc 
 const Rout = 1.0 # outer radius of the disc 
 # camera basics 
 const F = normalize(look_at - camera_position) # where the camera is looking at 
-const R = normalize(cross(F, world_up)) # horizontal axis of the camera
-const U = normalize(cross(R, F)) # vertical axis of the camera -- note that U should be already normalized, we do it for convenience
+const R = normalize(cross(F,world_up)) # horizontal axis of the camera
+const U = normalize(cross(F,R)) # vertical axis of the camera -- note that U should be already normalized, we do it for convenience
 const aspect_ratio = W / H # aspect ration 
 const scale = tan(fov / 2) 
 
@@ -43,67 +43,6 @@ end
 @inline function map_pixel(x::Int, N::Int)::Float64
     return ((x - 0.5) / N - 0.5) * 2.0
 end
-
-
-# # Stores the closest intersection distance for each pixel 
-# closest_t = fill(Inf, W, H)
-
-# for j in 1:H
-#     for i in 1:W 
-#         # normalize pixel coordinates
-#         u = map_pixel(i, W)
-#         v = map_pixel(j, H)
-
-#         # apply FOV scaling 
-#         px = u * aspect_ratio * scale
-#         py = -v * scale # minus sign so that the y-axis points up
-
-#         # ray direction 
-#         D = normalize(F + px*R + py*U)
-#         O = camera_position
-
-#         # ray-sphere intersection 
-#         # The equation for the sphere is given by: abs((camera_position + t * D)-sphere_position) = radius^2
-#         # where t is the length of the ray. 
-#         oc = O - sphere_position
-#         a = dot(D,D)
-#         b = 2 * dot(oc, D)
-#         c = dot(oc, oc) - sphere_radius^2
-#         discriminant = b^2 - 4*a*c
-
-#         if discriminant < 0
-#             image[j,i] = RGB(0,0,0)  # miss: black
-#         else
-#             image[j,i] = RGB(1,1,1)  # hit: white
-#             sqrt_discriminant = sqrt(discriminant)
-#             t1 = -b + sqrt_discriminant 
-#             t2 = -b - sqrt_discriminant 
-#             t_sphere = min(t1, t2) / (2 * a) # take the smallest positive t
-#             closest_t[j, i] = t_sphere 
-#         end
-
-#         # ray-disc intersection 
-#         if abs(D[2]) > 1e-5  
-#             t_disc = - camera_position[2] / D[2] # intersection with the y=0 plane 
-#             if t_disc > 0 && t_disc < closest_t[j,i]
-#                 P = camera_position + t_disc * D
-#                 rho = sqrt(P[1]^2 + P[3]^2) 
-#                 if Rin <= rho <= Rout
-#                     image[j,i] = RGB(1, 0, 0)  # hit: red
-#                 end 
-#             end
-#         end
-
-#         #TODO: add RK4 integrator and in each step test wheter the ray intersected the sphere or the disc 
-#         #NOTE: this means that we probably can not use the DifferentialEquations.jl because we need to test 
-#         # for the intersection in each step. 
-#         # This could still probably be done if we set some stopping condition but i don know how it would go with parallelization.
-#         #TODO: Add proper intersection functions for the sphere and the disc - since we are using spherical
-#         # coordinates, this for sphere can be done with a simple distance check. 
-#         #TODO: Derive the equations for the motion of one ray. Each ray represents a particle (photon) that we shoot
-#         # directly from the camera. Because of the curvature it will actually be possible to see behind the black hole.
-#     end 
-# end 
 
 function make_system(W::Int, H::Int)
     # This function creates array of rays with initial positions and velocities
@@ -208,8 +147,6 @@ function check_for_intersection(ray::Ray)
             return
         end          
     end
-
-
 end
 
 function main()
